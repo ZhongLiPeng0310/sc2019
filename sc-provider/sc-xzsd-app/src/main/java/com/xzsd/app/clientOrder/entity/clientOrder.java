c8bc55df6e9a540f458ef9a62c8538bae085b61f
ZhongLiPeng0310 <1253346029@qq.com>
2020-04-14 22:55:54 +0800

    图片上传

---
+++ sc-provider/sc-xzsd-app/src/main/java/com/xzsd/app/clientOrder/entity/clientOrder.java
@@ -0,0 +1,387 @@
+package com.xzsd.app.clientOrder.entity;
+
+import java.util.Date;
+
+/**
+ * 订单实体类
+ * @author zhong
+ * @date 2020-04-14
+ */
+public class clientOrder {
+    /**
+     * 订单编码
+     */
+    private String orderCode;
+    /**
+     * 客户编码
+     */
+    private String userCode;
+    /**
+     * 订单总价
+     */
+    private float orderMoney;
+    /**
+     * 订单状态  订单状态 0取消订单 1到货 2取消到货 3已取货 4取消已取货 5已完成（已完成未评价） 6取消已完成 7已付款 8已完成已评价
+     */
+    private int orderState;
+    /**
+     * 门店编码
+     */
+    private String storeCode;
+    /**
+     * 下单人
+     */
+    private String userName;
+    /**
+     * 下单人手机号码
+     */
+    private int phone;
+    /**
+     * 商品编码
+     */
+    private String goodsCode;
+    /**
+     * 商品名称
+     */
+    private String goodsName;
+    /**
+     * 购买数量
+     */
+    private int orderSum;
+    /**
+     * 商品售价
+     */
+    private float salePrice;
+    /**
+     * 商品成本价
+     */
+    private float costPrice;
+    /**
+     * 付款时间
+     */
+    private String createTime;
+    /**
+     * 付款时间区间
+     */
+    private String startPayTime;
+    private String endPayTime;
+    /**
+     * 角色 0管理员  1客户 2店长 3司机
+     */
+    private int role;
+    /**
+     * 店铺邀请码
+     */
+    private String inviteCode;
+    /**
+     * 当前登录人id
+     */
+    private String userId;
+    /**
+     * 作废标记 0为存在，1为作废
+     */
+    private int isDeleted;
+    /**
+     * 关键字
+     */
+    private String keyword;
+    /**
+     * 商品购买总数
+     */
+    private String sumGoods;
+    /**
+     * 商品图片
+     */
+    private String imagePath;
+    /**
+     * 详情地址
+     */
+    private String address;
+    /**
+     * 评价内容
+     */
+    private String appraiseDetail;
+    /**
+     * 评价星级
+     */
+    private int appraiseLevel;
+    /**
+     * 图片顺序
+     */
+    private int imageNum;
+    /**
+     * 更新人
+     * @return
+     */
+    private String updateName;
+    /**
+     * 更新时间
+     * @return
+     */
+    private Date updateTime;
+    /**
+     * 版本号
+     * @return
+     */
+    private String version;
+
+    /**
+     * 页码
+     * @return
+     */
+    private int pageSize;
+
+    /**
+     * 页数
+     * @return
+     */
+    private int pageNum;
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public float getOrderMoney() {
+        return orderMoney;
+    }
+
+    public void setOrderMoney(float orderMoney) {
+        this.orderMoney = orderMoney;
+    }
+
+    public int getOrderState() {
+        return orderState;
+    }
+
+    public void setOrderState(int orderState) {
+        this.orderState = orderState;
+    }
+
+    public String getStoreCode() {
+        return storeCode;
+    }
+
+    public void setStoreCode(String storeCode) {
+        this.storeCode = storeCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getGoodsCode() {
+        return goodsCode;
+    }
+
+    public void setGoodsCode(String goodsCode) {
+        this.goodsCode = goodsCode;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public int getOrderSum() {
+        return orderSum;
+    }
+
+    public void setOrderSum(int orderSum) {
+        this.orderSum = orderSum;
+    }
+
+    public float getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(float salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public float getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(float costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStartPayTime() {
+        return startPayTime;
+    }
+
+    public void setStartPayTime(String startPayTime) {
+        this.startPayTime = startPayTime;
+    }
+
+    public String getEndPayTime() {
+        return endPayTime;
+    }
+
+    public void setEndPayTime(String endPayTime) {
+        this.endPayTime = endPayTime;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public int getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(int isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getSumGoods() {
+        return sumGoods;
+    }
+
+    public void setSumGoods(String sumGoods) {
+        this.sumGoods = sumGoods;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAppraiseDetail() {
+        return appraiseDetail;
+    }
+
+    public void setAppraiseDetail(String appraiseDetail) {
+        this.appraiseDetail = appraiseDetail;
+    }
+
+    public int getAppraiseLevel() {
+        return appraiseLevel;
+    }
+
+    public void setAppraiseLevel(int appraiseLevel) {
+        this.appraiseLevel = appraiseLevel;
+    }
+
+    public int getImageNum() {
+        return imageNum;
+    }
+
+    public void setImageNum(int imageNum) {
+        this.imageNum = imageNum;
+    }
+
+    public String getUpdateName() {
+        return updateName;
+    }
+
+    public void setUpdateName(String updateName) {
+        this.updateName = updateName;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+}
